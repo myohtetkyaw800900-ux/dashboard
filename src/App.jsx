9f8e7df699d49c78f6340844f9338731fb3c9e2c
myohtetkyaw800900-ux <myohtetkyaw800900@gmail.com>
2025-09-25 11:09:18 +0630

    dashboard

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Dashboard from './components/Dashboard'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import ThemeContextProvider from './context/ThemeContextProvider'
+
+function App() {
+
+  return (
+    <ThemeContextProvider>
+    <div className='flex'>
+      <Sidebar />
+      <div className='grow ml-16 md:ml-64 h-full bg-gray-100 text-gray-900
+      dark:bg-gray-900 dark:text-white'>
+        <Navbar />
+        <div>
+          <Dashboard />
+        </div>
+      </div>
+    </div>
+    </ThemeContextProvider>
+  )
+}
+
+export default App
---
+++ src/assets/chartData.jsx
@@ -0,0 +1,25 @@
+export const dataLine = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August'],
+    datasets: [
+        {
+            label: 'Sales',
+            data: [65, 59, 80, 81, 56, 55, 60,76],
+            fill: false,
+            backgroundColor: 'blue',
+            borderColor: 'rgba(75,192,192,1)',
+        },
+    ],
+};
+
+export const dataBar= {
+    labels: ['Product A', 'Product B', 'Product C', 'Product D', 'Product E'],
+    datasets: [
+        {
+            label: 'Quantity',
+            data: [12, 19, 3, 5,7],
+            backgroundColor: 'rgba(153, 102, 255, 0.2)',
+            borderColor: '#008000',
+            borderWidth: 1,
+        },
+    ],
+};
